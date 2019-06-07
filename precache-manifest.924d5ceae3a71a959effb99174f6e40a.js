9878456b0054f6c2a2c02be265ab620d932309cb
Hiago Souza <hiagofss98@gmail.com>
2019-06-06 21:32:23 -0300

    Updates

---
+++ precache-manifest.924d5ceae3a71a959effb99174f6e40a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48481079e9f1a3f4622b90cf16bd139d",
+    "url": "/omnistackReact/index.html"
+  },
+  {
+    "revision": "4bf699c30c23ccdce33b",
+    "url": "/omnistackReact/static/css/main.54fac75f.chunk.css"
+  },
+  {
+    "revision": "0fbf26bc092b7c1ddf99",
+    "url": "/omnistackReact/static/js/2.e3379c23.chunk.js"
+  },
+  {
+    "revision": "4bf699c30c23ccdce33b",
+    "url": "/omnistackReact/static/js/main.d224af5a.chunk.js"
+  },
+  {
+    "revision": "fbdfd2ed9a95b69e79ca",
+    "url": "/omnistackReact/static/js/runtime~main.54ce8105.js"
+  }
+]);
